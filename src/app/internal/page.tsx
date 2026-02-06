d9fcdaf6f4c1dc6021846a137ad476dcc6047542
haseab <haseab.amin@gmail.com>
2026-02-05 19:22:45 -0800

    fix callback url

--- src/app/internal/page.tsx
+++ src/app/internal/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default function AdminPage() {
-  redirect("/admin/feedback");
+export default function InternalPage() {
+  redirect("/internal/feedback");
 }
