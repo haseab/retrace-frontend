b17566bc4668b3212c60f61d467392d32c210582
haseab <haseab.amin@gmail.com>
2026-04-03 15:17:31 -0700

    Update to v0.8.7

--- src/lib/retrace-release.ts
+++ src/lib/retrace-release.ts
@@ -1,4 +1,4 @@
-export const RETRACE_VERSION = "0.8.5";
+export const RETRACE_VERSION = "0.8.7";
 export const RETRACE_VERSION_LABEL = `v${RETRACE_VERSION}`;
 export const RETRACE_DOWNLOAD_FILENAME = `Retrace-${RETRACE_VERSION_LABEL}.dmg`;
 export const RETRACE_DOWNLOAD_URL =
